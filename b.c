6cb742f1c059fcdf8a0172fc97cac3524785277e
張修齊 <Floyd@111-243-48-14.dynamic.hinet.net>
2015-11-19 16:41:36 +0800

    another way of dec_to_bin, can run

--- b.c
+++ b.c
@@ -5,10 +5,12 @@
 #include <sys/ipc.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 //void dec_to_bin();
 
 struct datas{
+	int dec;
 	char dec_bin[25];
 	int frequency_1;
 	int length;
@@ -24,35 +26,53 @@ typedef struct mem_shared m_sh;
 typedef struct datas data;
 
 m_sh *shared_memory;
-//B storage
-data storage[30];
-int store_top = 0;
+	//B storage
+	data storage[30];
+	int store_top = 0;
+	int *SHM_dec_num;
+	char *SHM_bin_num;
+	int *SHM_flag;
 
 int main(int argc, char *argv[]){
+
 	int segment_id = atoi(argv[0]);
 	int flag;
-	int dec_num, mask =1;//for trans dec to bin
+	int dec_num;//for trans dec to bin
 	int i;//for loop
-	char reverse[20], result[20];//temp array to save answer
-	int index, frequency_1, num_index;//temp attribute of the answer
+	int index, frequency_1, num_index, answer;//temp attribute of the answer
 
 
 	shared_memory = (m_sh *) shmat(segment_id, NULL, 0);
+	SHM_dec_num = &shared_memory->dec_num;
+	SHM_bin_num = shared_memory->bin_num;
+	SHM_flag = &shared_memory->flag;
+
 	int switcher = 1;
 	while(switcher){
-		if( shared_memory->flag == 1){
-			/*printf("Step 1\n");
-			sprintf(shared_memory->bin_num, "00111");
-			shared_memory->flag = 2;*/
-			if(shared_memory->dec_num >=0 ){
-				index = 0;
-				sprintf(shared_memory->bin_num, "%d:", shared_memory->dec_num);
+		if( *SHM_flag == 1){
+			if(*SHM_dec_num >= 0){
+				frequency_1 = 0;
+				for(dec_num = *SHM_dec_num, index = 0, answer = 0; dec_num > 0; dec_num = dec_num >> 1,index++){
+					answer += (int)((dec_num & 1) * pow(10, index));
+					if((dec_num & 1) == 1) frequency_1++;
+				}
+				sprintf(SHM_bin_num, "%d:%d;", *SHM_dec_num, answer);
+				storage[store_top].frequency_1 = frequency_1;
+				storage[store_top].dec = *SHM_dec_num;
+				storage[store_top].length = (*SHM_dec_num/10) + index +3;
+				sprintf(storage[store_top].dec_bin, "%s", SHM_bin_num);
 				shared_memory->flag = 2;
 
 			}
-			else{
+			else if(*SHM_dec_num == -1){
 				sprintf(shared_memory->bin_num, "GGGGG");
 				shared_memory->flag = 2;
+			}
+			else if(*SHM_dec_num == -2){
+
+			}
+			else{
+
 			}
 		}
 	}
